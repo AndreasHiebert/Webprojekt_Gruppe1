25f29016112c3767c9d0d2880e7a15786ca7df29
Tim_Kierot <Tim.Kierot@stud.hshl.de>
2016-06-01 23:29:02 +0200

    finished test action

--- public_html/controller/loginAction.php
+++ public_html/controller/loginAction.php
@@ -4,19 +4,26 @@ $studeturl="../view/Homepage.html";
 $instructorurl="../view/Instructor.html";
 $errorurl="../view/Error404.html"
 
+$student = "student";
+$instructor = "instructor";
+
 $entered_email = $_GET["email_txt"];
 $entered_pass = $_GET["pass_txt"];
 
-
- if($entered_email==={user->getEmail}){
-   header("location:$instructorurl");
- } else {
-   header("location:fail.$errorurl");
- }
-
 {foreach from=$users item=$user}
-  if($_GET["email_txt"]==={user->getEmail}){
+  if($entered_email==={$user->getEmail()}){
+    if($entered_pass==={$user->getPassword()}){
+      if($user->getType==={$instructor}){
+        header("location:$instructorurl");
+      }else{
+        header("location:$studeturl");
+      }
+    } else{
+      header("location:fail.$errorurl");
+    }
+  } else{
+    header("location:fail.$errorurl");
+  }
 {/foreach}
 
 ?>
-($_GET["pass_txt"]===$userPass)
