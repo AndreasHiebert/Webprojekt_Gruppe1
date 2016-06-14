a6fa7b79b20b0c417e995daba49cc448644bc516
Gerold Hautmann <euwlolacc@gmx.de>
2016-06-14 11:20:13 +0200

    RegisterUser/RegisterInstructor prototype

--- public_html/controller/LoginController.php
+++ public_html/controller/LoginController.php
@@ -12,13 +12,35 @@ class LoginController {
     global $smarty;
     $smarty->display("../view/UserRegistration.html");
   }
+  
+  public function RegisterUser($name, $UserPassword, $EMail, $ActiveCourse){
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "webprojekt6";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+     
+    if($conn->connect_error){
+        die("COnnection failed: ".$conn->connect_error);
+    }
+    
+    $result = $mysqli->query("SELECT id FROM users ORDER BY name");
+    $id = $result->num_rows;
+     
+    $sql = "INSERT INTO users(id, name, password, email, activeCourse, fitnesspoints)
+            VALUES($name, $UserPassword, $EMail, $ActiveCourse, 0)";
 
-  public function getInstructorRegistrationForm(){
-    global $smarty;
-    $smarty->display("../view/InstructorRegistration.html");
+    if($conn->query($sql) === TRUE){
+        echo "New record created successfully";
+    }else{
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+     
+    $conn->close();
+             
   }
-
-  public function loginUser() {
+  
+    public function loginUser() {
       global $smarty;
       //$user = User::fromArray($_REQUEST);
       $smarty->display("../view/Homepage.html");
@@ -40,6 +62,38 @@ class LoginController {
           }
           */
     //  }
+  }
+
+  public function getInstructorRegistrationForm(){
+    global $smarty;
+    $smarty->display("../view/InstructorRegistration.html");
+  }
+  
+  public function RegisterInstructor($name, $Userpassword, $EMail){
+    $servername = "localhost";
+    $username = "username";
+    $password = "password";
+    $dbname = "webprojekt6";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    
+    if($conn->connect_error){
+        die("COnnection failed: ".$conn->connect_error);
+    }
+    
+    $result = $mysqli->query("SELECT id FROM instructors ORDER BY name");
+    $id = $result->num_rows;
+     
+    $sql = "INSERT INTO instructors(id, name, password, email)
+            VALUES($id+1, $name, $UserPassword, $EMail)";
+
+    if($conn->query($sql) === TRUE){
+        echo "New record created successfully";
+    }else{
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+     
+    $conn->close();
+             
   }
 
   public function loginInstructor() {
