8997e66bff06b25208578a30c5ece13910be71d4
Etienne Sandjon <etienne.sandjon@stud.hshl.de>
2016-06-16 15:07:08 +0200

    UserController bearbeitet

--- public_html/model/ModuleRepository.php
+++ public_html/model/ModuleRepository.php
@@ -16,13 +16,14 @@ class ModuleRepository {
     public static function saveModule($modul) {
         global $db;
 
-        $stmt = $db->prepare("INSERT into modules (name, abbrieviation, description, status, cp) "
-                . "values (:name, :abbrieviation, :description, :status, :cp)");
+        $stmt = $db->prepare("INSERT into modules (name, abbrieviation, description, status, cp, semester) "
+                . "values (:name, :abbrieviation, :description, :status, :cp, :semester)");
         $stmt->bindValue(':name', $user->getName(), PDO::PARAM_STR);
         $stmt->bindValue(':abbrieviation', $user->getAbbrieviation(), PDO::PARAM_STR);
         $stmt->bindValue(':description', $user->getDescription(), PDO::PARAM_STR);
         $stmt->bindValue(':status', $user->getStatus(), PDO::PARAM_INT);
         $stmt->bindValue(':cp', $user->getCp(), PDO::PARAM_INT);
+        $stmt->bindValue(':semester', $user->getSemester(), PDO::PARAM_INT);
 
         $stmt->execute();
     }
