9b8708bba4a97962597d65aed55d26bada1ecb58
rschiffers <rschiffers@xmail.net>
2016-06-17 16:04:53 +0200

    edited webprojekt_changeGrade.sql

--- public_html/database/webprojekt_changeGrades.sql
+++ public_html/database/webprojekt_changeGrades.sql
@@ -1,7 +1,7 @@
 drop table if exists webprojekt.grades;
 
 create table webprojekt.grades(
-	id int not null,
+	id int not null AUTO_INCREMENT,
 	grade double not null,
 	date TIMESTAMP default CURRENT_TIMESTAMP,
 	user_id int not null,
@@ -10,3 +10,10 @@ create table webprojekt.grades(
 	foreign key(user_id) references users(id),
 	foreign key(module_id) references modules(id)
 );
+
+insert into grades (grade, user_id, module_id) values(1.5, 1, 2);
+insert into grades (grade, user_id, module_id) values(2.0, 1, 4);
+insert into grades (grade, user_id, module_id) values(4.8, 1, 9);
+insert into grades (grade, user_id, module_id) values(2.8, 2, 4);
+insert into grades (grade, user_id, module_id) values(1.3, 2, 7);
+insert into grades (grade, user_id, module_id) values(5.0, 1, 4);
