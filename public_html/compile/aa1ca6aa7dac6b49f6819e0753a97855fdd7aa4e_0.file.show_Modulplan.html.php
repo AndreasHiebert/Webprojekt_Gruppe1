cf49ae43f100b3f012dd4d3da3f014fe9cdd263b
Etienne Sandjon <etienne.sandjon@stud.hshl.de>
2016-07-09 21:21:40 +0200

    Modulplan mobile Version überarbeitet

--- public_html/compile/aa1ca6aa7dac6b49f6819e0753a97855fdd7aa4e_0.file.show_Modulplan.html.php
+++ public_html/compile/aa1ca6aa7dac6b49f6819e0753a97855fdd7aa4e_0.file.show_Modulplan.html.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.29, created on 2016-07-09 20:45:46
+/* Smarty version 3.1.29, created on 2016-07-09 21:20:31
   from "C:\xampp\htdocs\WebProjekt\public_html\view\show_Modulplan.html" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.29',
-  'unifunc' => 'content_5781465af309c5_16330404',
+  'unifunc' => 'content_57814e7fea7d14_38363046',
   'file_dependency' => 
   array (
     'aa1ca6aa7dac6b49f6819e0753a97855fdd7aa4e' => 
     array (
       0 => 'C:\\xampp\\htdocs\\WebProjekt\\public_html\\view\\show_Modulplan.html',
-      1 => 1468089871,
+      1 => 1468092028,
       2 => 'file',
     ),
   ),
@@ -19,10 +19,10 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
   array (
   ),
 ),false)) {
-function content_5781465af309c5_16330404 ($_smarty_tpl) {
+function content_57814e7fea7d14_38363046 ($_smarty_tpl) {
 ?>
-<div id="modulebox">
-    <div id="modulplan_row" class="btn-group btn-group-justified " role="group" aria-label="...">
+<div id="modulebox" class="hidden-xs">
+    <div id="modulplan_row" class="btn-group btn-group-justified hidden-xs" role="group" aria-label="...">
         <?php
 $_from = $_smarty_tpl->tpl_vars['allModules']->value;
 if (!is_array($_from) && !is_object($_from)) {
@@ -38,7 +38,7 @@ $__foreach_module_0_saved_local_item = $_smarty_tpl->tpl_vars['module'];
         <?php if ($_smarty_tpl->tpl_vars['module']->value->getSemester() != $_smarty_tpl->tpl_vars['semester']->value) {?>
     </div>
     <br>
-    <div id = "semesterRow" class="btn-group btn-group-justified modulplan_group" role="group" aria-label="...">
+    <div id = "semesterRow" class="btn-group btn-group-justified hidden-xs" role="group" aria-label="...">
         <?php }?>
 
         <div class="btn-group" role="group">
@@ -103,6 +103,95 @@ $_smarty_tpl->tpl_vars['module'] = $__foreach_module_0_saved_local_item;
 }
 if ($__foreach_module_0_saved_item) {
 $_smarty_tpl->tpl_vars['module'] = $__foreach_module_0_saved_item;
+}
+?>
+    </div>
+</div>
+    
+    <!--Mobile Version-->
+  
+ <div id="modulebox" class="visible-xs">
+    <div id="modulplan_row" class="btn-group-vertical visible-xs" role="group" aria-label="...">
+        <?php
+$_from = $_smarty_tpl->tpl_vars['allModules']->value;
+if (!is_array($_from) && !is_object($_from)) {
+settype($_from, 'array');
+}
+$__foreach_module_1_saved_item = isset($_smarty_tpl->tpl_vars['module']) ? $_smarty_tpl->tpl_vars['module'] : false;
+$_smarty_tpl->tpl_vars['module'] = new Smarty_Variable();
+$_smarty_tpl->tpl_vars['module']->_loop = false;
+foreach ($_from as $_smarty_tpl->tpl_vars['module']->value) {
+$_smarty_tpl->tpl_vars['module']->_loop = true;
+$__foreach_module_1_saved_local_item = $_smarty_tpl->tpl_vars['module'];
+?>
+        <?php if ($_smarty_tpl->tpl_vars['module']->value->getSemester() != $_smarty_tpl->tpl_vars['semester']->value) {?>
+    </div>
+    <br>
+    <div id = "semesterRow" class="btn-group-vertical visible-xs" role="group" aria-label="...">
+        <?php }?>
+
+        <div class="btn-group" role="group">
+
+            <?php if ($_smarty_tpl->tpl_vars['module']->value->completedModule($_smarty_tpl->tpl_vars['module']->value->getId()) == true) {?>
+            <button id = "moduleButton"  name="<?php echo $_smarty_tpl->tpl_vars['module']->value->getName();?>
+" type="button" data-toggle="modal" data-target="#gradeModal<?php echo $_smarty_tpl->tpl_vars['module']->value->getId();?>
+" class="btn btn-success"><?php echo $_smarty_tpl->tpl_vars['module']->value->getName();?>
+<br>CP: <?php echo $_smarty_tpl->tpl_vars['module']->value->getCp();?>
+<br><b>Note: <?php echo $_smarty_tpl->tpl_vars['module']->value->completedModuleGrade($_smarty_tpl->tpl_vars['module']->value->getId());?>
+</b></button>
+            <?php } else { ?>
+            <button id = "moduleButton"  name="<?php echo $_smarty_tpl->tpl_vars['module']->value->getName();?>
+" type="button" data-toggle="modal" data-target="#gradeModal<?php echo $_smarty_tpl->tpl_vars['module']->value->getId();?>
+" class="btn btn-default"><?php echo $_smarty_tpl->tpl_vars['module']->value->getName();?>
+<br>CP: <?php echo $_smarty_tpl->tpl_vars['module']->value->getCp();?>
+</button>
+            <?php }?>
+
+
+            <div id="gradeModal<?php echo $_smarty_tpl->tpl_vars['module']->value->getId();?>
+" class="modal fade" role="dialog">
+                <div class="modal-dialog">
+
+
+                    <!--                        <input type="hidden" name="controller" value="ModuleController">
+                                            <input type="hidden" name="action" value="enterGrade">-->
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">Close</button>
+                            <h4 class="modal-title">Enter Grade for: <?php echo $_smarty_tpl->tpl_vars['module']->value->getName();?>
+</h4>
+                        </div>
+
+                        <div class="modal-body">
+                            <form action="index.php?controller=ModuleController&action=enterGrade" method="post">
+                                <div class="form-group">
+                                    <label for="grade">Grade:</label>
+                                    <input id="gradeInput" name="grade_input" class="form-control" type="text" title="grade" placeholder="Enter your grade">
+                                </div>
+
+                                <div class="form-group">
+                                    <input id="userId" name="user_id" class="form-control" type="hidden" value="<?php echo $_smarty_tpl->tpl_vars['currentUser']->value->getId();?>
+">
+                                </div>
+
+                                <div class="form-group">
+                                    <input id="moduleId" name="module_id" class="form-control" type="hidden" value="<?php echo $_smarty_tpl->tpl_vars['module']->value->getId();?>
+">
+                                </div>
+                                <button type="submit" class="btn btn-success">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php $_smarty_tpl->tpl_vars['semester'] = new Smarty_Variable($_smarty_tpl->tpl_vars['module']->value->getSemester(), null);
+$_smarty_tpl->ext->_updateScope->updateScope($_smarty_tpl, 'semester', 0);?>
+        <?php
+$_smarty_tpl->tpl_vars['module'] = $__foreach_module_1_saved_local_item;
+}
+if ($__foreach_module_1_saved_item) {
+$_smarty_tpl->tpl_vars['module'] = $__foreach_module_1_saved_item;
 }
 ?>
     </div>
--- public_html/js/Modulplan.js
+++ public_html/js/Modulplan.js
@@ -11,15 +11,15 @@ $('.btn-group-justified .btn').height(highestBox);
 
 });
 
-$(window).resize(function() {
-  if ($(window).width() < 770) {
-    $('#modulplan_row').removeClass('btn-group btn-group-justified');
-    $('#modulplan_row').addClass('btn-group-vertical');
-  } else {
-    $('#modulplan_row').addClass('btn-group btn-group-justified');
-    $('#modulplan_row').removeClass('btn-group-vertical');
-  }
-});
+//$(window).resize(function() {
+//  if ($(window).width() < 770) {
+//    $('#modulplan_row').removeClass('btn-group btn-group-justified');
+//    $('#modulplan_row').addClass('btn-group-vertical');
+//  } else {
+//    $('#modulplan_row').addClass('btn-group btn-group-justified');
+//    $('#modulplan_row').removeClass('btn-group-vertical');
+//  }
+//});
 
 $('#gradeModal').on('show.bs.modal', function (e) {
 		var name = e.relatedTarget.name;
