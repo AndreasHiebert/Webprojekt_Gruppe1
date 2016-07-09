98b6368bd3ea2156a459d2dc1b957bb6eddb605a
TimKierot <Tim.Kierot@stud.hshl.de>
2016-07-09 15:58:10 +0200

    fixed index

--- public_html/index.php
+++ public_html/index.php
@@ -84,8 +84,8 @@ $GLOBALS['currentUser'] = new User();
 $currentInstructor = new Instructor();
 $GLOBALS['currentInstructor'] = new Instructor();
 
-$currentUser = $loginUser;
-//$smarty->assign("currentUser", $currentUser,"global");
+$currentUser = $userObjects[0];
+$smarty->assign("currentUser", $currentUser);
 
 $currentInstructor = $instructorObjects[0];
 //$smarty->assign("currentInstructor", $currentInstructor);
