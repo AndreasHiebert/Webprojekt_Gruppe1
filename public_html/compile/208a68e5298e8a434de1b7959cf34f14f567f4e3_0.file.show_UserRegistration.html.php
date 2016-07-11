727ccce6c85216a824b9ec754b0eefd4d7ad3f78
TimKierot <Tim.Kierot@stud.hshl.de>
2016-07-11 13:39:21 +0200

    changed user reg and login view

--- public_html/compile/208a68e5298e8a434de1b7959cf34f14f567f4e3_0.file.show_UserRegistration.html.php
+++ public_html/compile/208a68e5298e8a434de1b7959cf34f14f567f4e3_0.file.show_UserRegistration.html.php
@@ -1,17 +1,17 @@
 <?php
-/* Smarty version 3.1.29, created on 2016-07-11 13:22:02
+/* Smarty version 3.1.29, created on 2016-07-11 13:36:26
   from "C:\xampp\htdocs\WebProjekt\public_html\view\show_UserRegistration.html" */
 
 if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
   'has_nocache_code' => false,
   'version' => '3.1.29',
-  'unifunc' => 'content_5783815a4f85d4_84708617',
+  'unifunc' => 'content_578384ba299ee5_59755801',
   'file_dependency' => 
   array (
     '208a68e5298e8a434de1b7959cf34f14f567f4e3' => 
     array (
       0 => 'C:\\xampp\\htdocs\\WebProjekt\\public_html\\view\\show_UserRegistration.html',
-      1 => 1468236120,
+      1 => 1468236984,
       2 => 'file',
     ),
   ),
@@ -19,14 +19,12 @@ if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl,
   array (
   ),
 ),false)) {
-function content_5783815a4f85d4_84708617 ($_smarty_tpl) {
+function content_578384ba299ee5_59755801 ($_smarty_tpl) {
 ?>
-
-
   <form role="form" action="index.php?controller=LoginController&action=RegisterUser" method="post">
     <div class="form-group">
       <label for="username">Name:</label>
-      <input type="text" id="username" class="form-control" name="username_txt" placeholder="Username:" maxlength="30" required>
+      <input type="text" id="username" class="form-control" name="username_txt" placeholder="Name eingeben..." maxlength="30" required>
     </div>
 
     <div class="form-group">
@@ -54,6 +52,6 @@ function content_5783815a4f85d4_84708617 ($_smarty_tpl) {
     <div class="form-group">
         <button type="submit" id="registration_btn" title="Bestätigen Sie die Angaben" class="btn btn-default" value="Registrieren">Registriern!</button>
     </div>
-    </form>
+  </form>
 <?php }
 }
